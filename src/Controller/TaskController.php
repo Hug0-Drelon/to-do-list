b4f4e558af29d68f02a1fef547587c7d544ff9af
Hug0-Drelon <hugo.drelon@gmail.com>
2021-04-28 12:22:48 +0200

    add method task crud

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @Route("/tasks", name="task_")
@@ -14,7 +18,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class TaskController extends AbstractController
 {
     /**
-     * @Route("/", name"show_all", methods={"GET"})
+     * @Route("/", name="show_all", methods={"GET"})
      */
     public function showAll(TaskRepository $taskRepository)
     {
@@ -29,5 +33,28 @@ class TaskController extends AbstractController
     public function show(Task $task)
     {
         return $this->json($task);
+    }
+
+    /**
+     * @Route("/", name="add", methods={"POST"})
+     */
+    public function add(EntityManagerInterface $em, Request $request, SerializerInterface $serializer, ValidatorInterface $validator)
+    {
+        $jsonResponse = $request->getContent();
+
+        $task = $serializer->deserialize($jsonResponse, Task::class, 'json');
+
+        $errors = $validator->validate($task);
+
+        if (count($errors)) {
+            $errorsString = (string) $errors;
+
+            return $this->json($errorsString, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $em->persist($task);
+        $em->flush();
+
+        return $this->json($task, Response::HTTP_CREATED, ['Location' => $this->generateUrl('task_show', ['id' => $task->getId()])]);
     }
 }
