4f9a3f3835f0e4a632c3a974e6e938645649ba8a
Hug0-Drelon <hugo.drelon@gmail.com>
2021-04-28 12:33:02 +0200

    task crud done

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -56,5 +57,38 @@ class TaskController extends AbstractController
         $em->flush();
 
         return $this->json($task, Response::HTTP_CREATED, ['Location' => $this->generateUrl('task_show', ['id' => $task->getId()])]);
+    }
+
+    /**
+     * @Route("/{id<\d+>}", name="update", methods={"PUT", "PATCH"})
+     */
+    public function update(Task $task, Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $em)
+    {
+        $jsonContent = $request->getContent();
+
+        $serializer->deserialize($jsonContent, Task::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $task]);
+        
+        $errors = $validator->validate($task);
+
+        if (count($errors)) {
+            $errorsString = (string) $errors;
+
+            return $this->json($errorsString, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $em->flush();
+
+        return $this->json($task, Response::HTTP_CREATED, ['Location' => $this->generateUrl('task_show', ['id' => $task->getId()])]);
+    }
+
+    /**
+     * @Route("/{id<\d+>}", name="delete", methods={"DELETE"})
+     */
+    public function delete(Task $task, EntityManagerInterface $em)
+    {
+        $em->remove($task);
+        $em->flush();
+
+        return $this->json(Response::HTTP_OK);
     }
 }
