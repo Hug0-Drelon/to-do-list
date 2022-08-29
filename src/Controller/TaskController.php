d068350d3e4dec88a6416d1ac80f39aec37edeef
Hug0-Drelon <hugo.drelon@gmail.com>
2022-08-29 08:50:01 +0200

    Use attributes instead of annotations for routes.

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -14,14 +14,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-/**
- * @Route("/api/tasks", name="tasks_")
- */
+#[Route("/api/tasks", name: "tasks_")]
 class TaskController extends AbstractController
 {
-    /**
-     * @Route("/", name="show_all", methods={"GET"})
-     */
+    #[Route("/", name: "show_all", methods: ["GET"])]
     public function showAll(TaskRepository $taskRepository)
     {
         $tasks = ['result' => $taskRepository->findWithCategoryAndSubtasks()];
@@ -29,9 +25,7 @@ class TaskController extends AbstractController
         return $this->json($tasks, Response::HTTP_OK, [], ['groups' => 'task_get']);
     }
 
-    /**
-     * @Route("/{id<\d+>}", name="show_one", methods={"GET"})
-     */
+    #[Route("/{id<\d+>}", name: "show_one", methods: ["GET"])]
     public function showOne(int $id, TaskRepository $taskRepository)
     {
         $task = $taskRepository->findOneWithCategoryAndSubtasks($id);
@@ -52,9 +46,7 @@ class TaskController extends AbstractController
         return $this->json($resultArray, Response::HTTP_OK, [], ['groups' => 'task_get']);
     }
 
-    /**
-     * @Route("/", name="add", methods={"POST"})
-     */
+    #[Route("/", name: "add", methods: ["POST"])]
     public function add(EntityManagerInterface $em, Request $request, ErrorsHandler $errorsHandler, ValidatorInterface $validator, CategoryRepository $categoryRepository)
     {
         $taskName = $request->request->get('name');
@@ -78,9 +70,7 @@ class TaskController extends AbstractController
         return $this->json(['result' => $task], Response::HTTP_CREATED, ['Location' => $this->generateUrl('tasks_show_one', ['id' => $task->getId()])], ['groups' => 'task_get']);
     }
 
-    /**
-     * @Route("/{id<\d+>}", name="update", methods={"PUT", "PATCH"})
-     */
+    #[Route("/{id<\d+>}", name: "update", methods: ["PUT", "PATCH"])]
     public function update(int $id, Request $request, ErrorsHandler $errorsHandler, ValidatorInterface $validator, EntityManagerInterface $em,CategoryRepository $categoryRepository, TaskRepository $taskRepository)
     {
         $task = $taskRepository->findOneWithCategoryAndSubtasks($id);
@@ -115,9 +105,7 @@ class TaskController extends AbstractController
         return $this->json([], Response::HTTP_NO_CONTENT, ['Location' => $this->generateUrl('tasks_show_one', ['id' => $task->getId()])], ['groups' => 'task_get']);
     }
 
-    /**
-     * @Route("/{id<\d+>}", name="delete", methods={"DELETE"})
-     */
+    #[Route("/{id<\d+>}", name: "delete", methods: ["DELETE"])]
     public function delete(int $id, EntityManagerInterface $em, TaskRepository $taskRepository)
     {
         $task = $taskRepository->findOneWithCategoryAndSubtasks($id);
